14e290424d1ae91033753d210e1b291ccaa8c75d
刘庆 <1603527727@qq.com>
2023-07-12 12:40:36 +0800

    feat: 环境变量和模式

--- src/views/main/index.vue
+++ src/views/main/index.vue
@@ -1,4 +1,7 @@
 <template>
+
+  <h1>VITE_USER_NODE_ENV: {{envStr.VITE_USER_NODE_ENV}}</h1>
+  <h1>MODE: {{envStr.MODE}}</h1>
   <a-button type="primary">Primary Button</a-button>
   <a-button>Default Button</a-button>
   <a-button type="dashed">Dashed Button</a-button>
@@ -6,10 +9,25 @@
   <a-button type="link">Link Button</a-button>
 </template>
 
-<script lang="ts">
-export default {
-  name: 'index',
-};
+<script setup lang="ts">
+let envStr = import.meta.env;
+interface UserInfo {
+  name: string,
+  age: number,
+  gender: string,
+  hobby: string[],
+}
+
+let user: UserInfo = {
+  name: 'LinQ',
+  age: 20,
+  gender: 'male',
+  hobby: ['music'],
+}
+
+console.log('user: ', user);
+// console.log('import.meta: ', import.meta);
+// console.log('import.meta.env: ', import.meta.env);
 </script>
 
 <style scoped>
